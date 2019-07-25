585aa03bb9f7e3144a00a84e00bc7a224cd106f1
Matheus A <malencar@pm.me>
2019-07-25 16:16:58 -0300

    feat: also fetch exchanges in batches

--- src/utils/get-tokens-with-badge.js
+++ src/utils/get-tokens-with-badge.js
@@ -1,7 +1,8 @@
 import _arbitrableAddressList from '../constants/abis/arbitrable-address-list.json'
 import _tokensView from '../constants/abis/tokens-view.json'
-import { T2CR_ADDRESSES, ERC20_BADGE_ADDRESSES, TOKENS_VIEW_ADDRESSES } from '../constants/index'
-import { getContract, getTokenDecimals, getTokenExchangeAddressFromFactory } from './index'
+import _exchangeView from '../constants/abis/exchange-view.json'
+import { T2CR_ADDRESSES, ERC20_BADGE_ADDRESSES, TOKENS_VIEW_ADDRESSES, EXCHANGE_VIEW_ADDRESSES, FACTORY_ADDRESSES } from '../constants/index'
+import { getContract, getTokenDecimals } from './index'
 
 const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 const ZERO_ID = '0x0000000000000000000000000000000000000000000000000000000000000000'
@@ -16,29 +17,39 @@ export default async function getTokensWithBadge(library, networkId) {
       ZERO_ADDRESS
     )
     const tokensView = getContract(TOKENS_VIEW_ADDRESSES[networkId], _tokensView, library, ZERO_ADDRESS)
+    const exchangeView = getContract(EXCHANGE_VIEW_ADDRESSES[networkId], _exchangeView, library, ZERO_ADDRESS)
     const addressesWithBadge = (await arbitrableAddressList.queryAddresses(ZERO_ADDRESS, 100, filter, true))[0].filter(
       address => address !== ZERO_ADDRESS
     )
     const tokenIDs = (await tokensView.getTokensIDsForAddresses(T2CR_ADDRESSES[networkId], addressesWithBadge)).filter(tokenID => tokenID !== ZERO_ID)
     const tokensInfo = (await tokensView.getTokens(T2CR_ADDRESSES[networkId], tokenIDs))
-      .filter(tokenInfo => tokenInfo[3] !== '0x0000000000000000000000000000000000000000')
+      .filter(tokenInfo => tokenInfo[3] !== ZERO_ADDRESS)
       .reduce((acc, curr) => ({
         ...acc,
         [curr[3]]: curr
       }), {})
 
+    const addresses = Object.keys(tokensInfo)
+    const exchangeAddresses = (await exchangeView.getExchanges(FACTORY_ADDRESSES[networkId], addresses)).slice(0, addresses.length)
+    addresses.forEach((address, i) => {
+      if (exchangeAddresses[i] !== ZERO_ADDRESS) tokensInfo[address].exchangeAddress = exchangeAddresses[i]
+    })
+
+    // We can't fetch all token decimal places information at once (as done with exchange addresses and token information)
+    // because some tokens do not implement the decimals() function.
+    // We can't use assembly with STATICCALL (as done in EIP-165) to detect this as contracts with function callbacks can
+    // cause false positives.
     await Promise.all(
-      Object.keys(tokensInfo).map(async address => {
+      addresses.map(async address => {
         try {
           tokensInfo[address].decimals = await getTokenDecimals(address, library)
-          tokensInfo[address].exchangeAddress = await getTokenExchangeAddressFromFactory(address, networkId, library)
           return null
         } catch (err) {
           console.warn('Could not fetch token information for token of address ' + address)
         }
       })
     )
-    Object.keys(tokensInfo).forEach(address => {
+    addresses.forEach(address => {
       if (!tokensInfo[address].decimals || !tokensInfo[address].exchangeAddress)
         delete tokensInfo[address]
     })
