62c3cdf2344fb8d31d2089db466d573f3e1f278a
mtsalenc <matheus@kleros.io>
2021-07-24 17:19:32 +0200

    feat: add mycrypto, uni, zapper lists

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -5,6 +5,9 @@ const COINGECKO_LIST = 'https://tokens.coingecko.com/uniswap/all.json'
 const COMPOUND_LIST = 'https://raw.githubusercontent.com/compound-finance/token-list/master/compound.tokenlist.json'
 const GEMINI_LIST = 'https://www.gemini.com/uniswap/manifest.json'
 const KLEROS_LIST = 't2crtokens.eth'
+const ZAPPERFI_LIST = 'https://zapper.fi/api/token-list'
+const MYCRYPTO_LIST = 'https://uniswap.mycryptoapi.com/'
+const UNISWAP_DEFAULT_LIST = 'https://gateway.ipfs.io/ipns/tokens.uniswap.org'
 export const OPTIMISM_LIST = 'https://static.optimism.io/optimism.tokenlist.json'
 const ROLL_LIST = 'https://app.tryroll.com/tokens.json'
 const SET_LIST = 'https://raw.githubusercontent.com/SetProtocol/uniswap-tokenlist/main/set.tokenlist.json'
@@ -26,6 +29,9 @@ export const DEFAULT_LIST_OF_LISTS: string[] = [
   KLEROS_LIST,
   OPTIMISM_LIST,
   GEMINI_LIST,
+  ZAPPERFI_LIST,
+  MYCRYPTO_LIST,
+  UNISWAP_DEFAULT_LIST,
   ...UNSUPPORTED_LIST_URLS, // need to load unsupported tokens as well
 ]
 
